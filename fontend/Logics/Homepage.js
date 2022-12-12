4dacf3e85caa8d65b52eda5be2c8b73ca49c036e
FishyDeveloper <ikanfugu511@gmail.com>
2022-12-12 23:53:06 +0700

    Added something....

--- fontend/Logics/Homepage.js
+++ fontend/Logics/Homepage.js
@@ -2,11 +2,16 @@ const Navbar = $(".navbar");
 
 ScrollTrigger.create({
     start:'25px',
-    markers:true,
     onEnter:() => {
         gsap.to(Navbar,{backgroundColor:'#103396',duration:0.1})
     },
     onLeaveBack:() => {
         gsap.to(Navbar,{backgroundColor:'#10339600',duration:0.1})
     }
+})
+
+$("#home").click(() => {
+    $('html,body').animate({
+        scrollTop:0
+    })
 })
