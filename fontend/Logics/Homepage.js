b6aaca1002f0203a3e6fdba5d37f606285c92310
FishyDeveloper <ikanfugu511@gmail.com>
2022-12-18 20:58:13 +0700

    added some features

--- fontend/Logics/Homepage.js
+++ fontend/Logics/Homepage.js
@@ -15,3 +15,19 @@ $("#home").click(() => {
         scrollTop:0
     })
 })
+
+$(".activity_card").hover((e) => {
+    let el = e.target
+    
+    if (el.className != 'activity_card'){
+        el = el.parentElement
+    }
+    gsap.to(el,{y:'-=5',boxShadow: '3px 3px 10px 1px rgb(204, 204, 204) '})
+}, (e) => {
+    let el = e.target
+    
+    if (el.className != 'activity_card'){
+        el = el.parentElement
+    }
+    gsap.to(el,{y:'0',boxShadow: '1px 1px 10px 0px rgb(204, 204, 204) '})
+})
