f5cea4426a2f8eb128c1fcf4acea42b100f16312
FishyDeveloper <ikanfugu511@gmail.com>
2022-12-21 05:31:18 +0700

    minor edits

--- fontend/Logics/Homepage.js
+++ fontend/Logics/Homepage.js
@@ -22,12 +22,12 @@ $(".activity_card").hover((e) => {
     if (el.className != 'activity_card'){
         el = el.parentElement
     }
-    gsap.to(el,{y:'-=5',boxShadow: '3px 3px 10px 1px rgb(204, 204, 204) '})
+    gsap.to(el,{y:'-=5',boxShadow: '4px 4px 10px hsl(0deg 0% 0% / 0.085),5px 5px 10px hsl(0deg 0% 0% / 0.085)'})
 }, (e) => {
     let el = e.target
     
     if (el.className != 'activity_card'){
         el = el.parentElement
     }
-    gsap.to(el,{y:'0',boxShadow: '1px 1px 10px 0px rgb(204, 204, 204) '})
+    gsap.to(el,{y:'0',boxShadow: '2px 2px 10px hsl(0deg 0% 0% / 0.085),3px 3px 10px hsl(0deg 0% 0% / 0.085)'})
 })
