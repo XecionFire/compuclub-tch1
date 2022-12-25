8f6858433fc8898d462ca6d75c8f7cf424cef0a2
FishyDeveloper <ikanfugu511@gmail.com>
2022-12-25 21:03:37 +0700

    Dev Update

--- fontend/Logics/Homepage.js
+++ fontend/Logics/Homepage.js
@@ -20,14 +20,18 @@ $(".activity_card").hover((e) => {
     let el = e.target
     
     if (el.className != 'activity_card'){
-        el = el.parentElement
+        el = el.closest('activity_card')
     }
-    gsap.to(el,{y:'-=5',boxShadow: '4px 4px 10px hsl(0deg 0% 0% / 0.085),5px 5px 10px hsl(0deg 0% 0% / 0.085)'})
+    gsap.to(el,{y:'-=5',boxShadow: '4px 4px 10px hsl(0deg 0% 0% / 0.085),5px 5px 10px hsl(0deg 0% 0% / 0.075)'})
 }, (e) => {
     let el = e.target
     
     if (el.className != 'activity_card'){
-        el = el.parentElement
+        el = el.closest('activity_card')
     }
     gsap.to(el,{y:'0',boxShadow: '2px 2px 10px hsl(0deg 0% 0% / 0.085),3px 3px 10px hsl(0deg 0% 0% / 0.085)'})
+})
+
+$('.menu-wrapper').children().each(function() {
+    console.log(this);
 })
